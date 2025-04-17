9c91775a10304a0a9622029bcf4a7e1cf23c3fed
AnchalMish9630 <anchalmish2025@gmail.com>
2025-04-17 23:15:22 +0530

    Added input box and otp functionality

--- src/SelectBox.jsx
+++ src/SelectBox.jsx
@@ -1,13 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const SelectBox = () => {
+const SelectBox = ({ label }) => {
+  const [inputVal, setInputVal] = useState(["", "", "", ""]);
 
-  const [currentIndex, setCurrentIndex] = useState("")
+  const handleChange = (index, value) => {
+    // debugger
+    const updatedArray = [...inputVal];
+    updatedArray[index] = value;
+    setInputVal(updatedArray);
+  };
+
+  const allFilled = inputVal.every((val) => val !== "");
+  const handleSubmit = () => {
+    alert("OTP submitted");
+  };
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div style={{ display: "flex", gap: "1rem" }}>
+      {[...Array(4)].map((_, index) => (
+        <div key={index}>
+          <label>
+            <input
+              type="text"
+              style={{ padding: "12px", width: "20px" }}
+              value={inputVal[index]}
+              onChange={(e) => {
+                const re = /^[0-9\b]+$/;
 
-export default SelectBox
+                // if value is not blank, then test the regex
+
+                if (e.target.value === "" || re.test(e.target.value)) {
+                  handleChange(index, e.target.value);
+                } else {
+                  alert("Enter value between 1-9");
+                }
+              }}
+              disabled={index !== 0 && inputVal[index - 1] === ""}
+            />
+            <span>{label}</span>
+          </label>
+        </div>
+      ))}
+
+      <button disabled={!allFilled} onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default SelectBox;
