5e1c5cdd7185208995128d505576ef1444dd496c
AnchalMish9630 <anchalmish2025@gmail.com>
2025-04-18 21:13:01 +0530

    Select box changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import Carouseal from './Carouseal'
+import SelectBox from './SelectBox'
 
 function App() {
   return (
     <>
-    <div>
-    <Carouseal />
+    <div style={{
+     margin: "40px",
+     display: "flex", flexDirection: "row", gap: "1rem", alignItems: "center"}}>
+    {/* <Carouseal /> */}
+    <SelectBox label={""} />
+    {/* <SelectBox label={""} />
+    <SelectBox label={""} />
+    <SelectBox label={""} /> */}
+     
     </div>
   </>
   )
